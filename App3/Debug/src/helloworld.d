3b7f12f1dead0623ab2b582817c86ebbf2bf27ce
Dimiter <Q-point@users.noreply.github.com>
2020-07-20 09:20:48 -0400

    Add files via upload

---
+++ App3/Debug/src/helloworld.d
@@ -0,0 +1,104 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_printf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_types.h \
+ ../../App_bsp/psu_cortexa53_0/include/xparameters.h \
+ ../../App_bsp/psu_cortexa53_0/include/xparameters_ps.h \
+ ../../App_bsp/psu_cortexa53_0/include/bspconfig.h \
+ ../../App_bsp/psu_cortexa53_0/include/xvtc.h \
+ ../../App_bsp/psu_cortexa53_0/include/xvtc_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_io.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_printf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xpseudo_asm.h \
+ ../../App_bsp/psu_cortexa53_0/include/xreg_cortexa53.h \
+ ../../App_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_assert.h \
+ ../../App_bsp/psu_cortexa53_0/include/xstatus.h ../src/vga_modes.h \
+ ../../App_bsp/psu_cortexa53_0/include/xv_tpg.h \
+ ../../App_bsp/psu_cortexa53_0/include/xv_tpg_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/xvidc.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/sleep.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf_clk.h \
+ ../src/xdpdma_video_example.h \
+ ../../App_bsp/psu_cortexa53_0/include/xparameters.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdpdma.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdpdma_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/xvidc.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xscugic.h \
+ ../../App_bsp/psu_cortexa53_0/include/xscugic_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_exception.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdppsu.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdppsu_hw.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_printf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_types.h:
+
+../../App_bsp/psu_cortexa53_0/include/xparameters.h:
+
+../../App_bsp/psu_cortexa53_0/include/xparameters_ps.h:
+
+../../App_bsp/psu_cortexa53_0/include/bspconfig.h:
+
+../../App_bsp/psu_cortexa53_0/include/xvtc.h:
+
+../../App_bsp/psu_cortexa53_0/include/xvtc_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_io.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_printf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xpseudo_asm.h:
+
+../../App_bsp/psu_cortexa53_0/include/xreg_cortexa53.h:
+
+../../App_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_assert.h:
+
+../../App_bsp/psu_cortexa53_0/include/xstatus.h:
+
+../src/vga_modes.h:
+
+../../App_bsp/psu_cortexa53_0/include/xv_tpg.h:
+
+../../App_bsp/psu_cortexa53_0/include/xv_tpg_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/xvidc.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/sleep.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf_clk.h:
+
+../src/xdpdma_video_example.h:
+
+../../App_bsp/psu_cortexa53_0/include/xparameters.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdpdma.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdpdma_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/xvidc.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xscugic.h:
+
+../../App_bsp/psu_cortexa53_0/include/xscugic_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_exception.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdppsu.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdppsu_hw.h:
---
+++ App3/Debug/src/xdpdma_video_example.d
@@ -0,0 +1,89 @@
+src/xdpdma_video_example.o src/xdpdma_video_example.o: \
+ ../src/xdpdma_video_example.c \
+ ../../App_bsp/psu_cortexa53_0/include/xil_exception.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_types.h \
+ ../../App_bsp/psu_cortexa53_0/include/xpseudo_asm.h \
+ ../../App_bsp/psu_cortexa53_0/include/xreg_cortexa53.h \
+ ../../App_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h \
+ ../../App_bsp/psu_cortexa53_0/include/bspconfig.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_printf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xparameters.h \
+ ../../App_bsp/psu_cortexa53_0/include/xparameters_ps.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_cache.h \
+ ../src/xdpdma_video_example.h \
+ ../../App_bsp/psu_cortexa53_0/include/xparameters.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdpdma.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdpdma_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_io.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_printf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xvidc.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_assert.h \
+ ../../App_bsp/psu_cortexa53_0/include/xstatus.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/sleep.h \
+ ../../App_bsp/psu_cortexa53_0/include/xscugic.h \
+ ../../App_bsp/psu_cortexa53_0/include/xscugic_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/xil_exception.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdppsu.h \
+ ../../App_bsp/psu_cortexa53_0/include/xdppsu_hw.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf.h \
+ ../../App_bsp/psu_cortexa53_0/include/xavbuf_clk.h
+
+../../App_bsp/psu_cortexa53_0/include/xil_exception.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_types.h:
+
+../../App_bsp/psu_cortexa53_0/include/xpseudo_asm.h:
+
+../../App_bsp/psu_cortexa53_0/include/xreg_cortexa53.h:
+
+../../App_bsp/psu_cortexa53_0/include/xpseudo_asm_gcc.h:
+
+../../App_bsp/psu_cortexa53_0/include/bspconfig.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_printf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xparameters.h:
+
+../../App_bsp/psu_cortexa53_0/include/xparameters_ps.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_cache.h:
+
+../src/xdpdma_video_example.h:
+
+../../App_bsp/psu_cortexa53_0/include/xparameters.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdpdma.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdpdma_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_io.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_printf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xvidc.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_assert.h:
+
+../../App_bsp/psu_cortexa53_0/include/xstatus.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/sleep.h:
+
+../../App_bsp/psu_cortexa53_0/include/xscugic.h:
+
+../../App_bsp/psu_cortexa53_0/include/xscugic_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/xil_exception.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdppsu.h:
+
+../../App_bsp/psu_cortexa53_0/include/xdppsu_hw.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf.h:
+
+../../App_bsp/psu_cortexa53_0/include/xavbuf_clk.h:
